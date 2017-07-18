a84f872ddb71fd8e7923e56a861226b3f71dcf85
Pyrotelekinetic <Pyrotelekinetic@users.noreply.github.com>
2017-07-18 16:27:41 -0700

    Create wide.py
    It's good I swear.

---
+++ cogs/wide.py
@@ -0,0 +1,7 @@
+
+{
+  "title": "Wide",
+  "description": "It makes it wide-text, whatever that is.",
+	"author": "Pyrotelekinetic",
+	"link": "https://raw.githubusercontent.com/Pyrotelekinetic/Pyros-Cogs/master/wide.py"
+}
