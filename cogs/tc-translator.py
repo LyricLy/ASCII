624c1a16f2e6c561eb010a1beaa5f353323aafba
deepakraj-tc <34530901+deepakraj-tc@users.noreply.github.com>
2018-01-08 22:19:16 +0530

    added "dependencies": [ "googletrans" ]

--- cogs/tc-translator.py
+++ cogs/tc-translator.py
@@ -2,5 +2,6 @@
     "title": "Translator",
     "description": "A simple translator cog to translate every message you send to your desired language while translation mode is on by editing the message you sent. Note: It requires googletrans module for translating messages. If your app is deployed on heroku or same, you may put googletrans==2.2.0 to requirements.txt file or if you're using it on local machine having python, you can also install it using pip (pip install googletrans).",
     "author": "◻ | The Cosmos",
-    "link": "https://raw.githubusercontent.com/the-cosmos/discord-selfbot-cogs/master/tc-translator.py"
+    "link": "https://raw.githubusercontent.com/the-cosmos/discord-selfbot-cogs/master/tc-translator.py",
+    "dependencies": [ "googletrans" ]
 }
